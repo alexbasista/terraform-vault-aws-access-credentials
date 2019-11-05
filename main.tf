ed5909203631d5b86b0f14ddac2f97299f33047a
alexbasista <alex.basista@gmail.com>
2019-11-05 07:21:02 -0500

    comment out provider

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "vault" {
-  address = "https://<my-vault-hostname>:8200"
-  # token needs to be set via VAULT_TOKEN environment variable
-  # if using Vault namespaces set here via 'namespace' argument
-}
+# provider "vault" {
+#   address = "https://<my-vault-hostname>:8200"
+#   # token needs to be set via VAULT_TOKEN environment variable
+#   # if using Vault namespaces set here via 'namespace' argument
+# }
 
 data "vault_aws_access_credentials" "creds" {
   backend = var.aws_secrets_engine_name
